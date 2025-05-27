96fd3cc70239cc4ae8c4d37e1b70db6ddf993b3c
hGauna <undrbug@gmail.com>
2025-05-26 22:51:01 -0300

    agregue rutas sin seguridad

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Main from './components/Main'
-import Section from './components/Section'
-import Article from './components/Article'
-import Aside from './components/Aside'
-import Footer from './components/Footer'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx'
+import Login from './components/Login.jsx'
+import Dashboard from './components/Dashboard.jsx';
+import Register from './components/Register.jsx';
+import Shop from './components/Shop.jsx';
 import './App.css'
 
 function App() {
@@ -15,14 +17,31 @@ function App() {
         <h3>BienHelada.com.ar</h3>
       </div>
       <Header />
-      <Main>
-        <Section title="Bienvenidos a BienHelada.com.ar">
-          <Article title="Artículo 2" content="mostrar los productos" />
-        </Section>
-      </Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/deals" element={<Deals />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/shop" element={<Shop />} />
+          {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
+          {/* <Route path="/checkout" element={<Checkout />} /> */}          
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* ruta privada, falta implementar */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       <Footer />
     </Router>
   )
 }
 
 export default App
+
+      // <Main>
+      //   <Section title="Bienvenidos a BienHelada.com.ar">
+      //     <Article content={<Card />} />
+      //     <Article content={<Card />} />
+      //     <Article content={<Card />} />
+      //     <Article content={<Card />} />
+      //   </Section>
+      // </Main>
