8a022a37643e1d7c8c18c433bdcab80190583094
hGauna <undrbug@gmail.com>
2025-05-26 23:01:56 -0300

    Componente que todavia falta implementar para el admin del sitio

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Bienvenido al panel de control.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
+    
