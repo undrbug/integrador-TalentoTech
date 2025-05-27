954d9b5eb9cd898a2c53fcbb1a0c450a99902d11
hGauna <undrbug@gmail.com>
2025-05-26 22:54:31 -0300

    se agregaron Links o enlaces al navbar y un footer basico

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Container } from 'react-bootstrap';
 
 const Footer = () => (
+
   <footer className=" footer bg-dark text-light py-3 mt-auto">
     <Container className="text-center">
       <p className="mb-0">© {new Date().getFullYear()} BienHelada.com.ar - Todos los derechos reservados.</p>
     </Container>
   </footer>
+
 );
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <Navbar bg="dark" variant="dark" expand="lg" className='radius'>
       <Container>
-        <Navbar.Brand href="#">MiApp</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Soltec(logo)</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#features">Características</Nav.Link>
-            <Nav.Link href="#contact">Contacto</Nav.Link>
+          <Nav className="justify-content-end flex-grow-1">
+            <Nav.Link as={Link} to="/deals">Ofertas</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+            <Nav.Link as={Link} to="/about">Acerca de</Nav.Link>
+            <Nav.Link as={Link} to="/login">Iniciar Sesión</Nav.Link>
+            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
