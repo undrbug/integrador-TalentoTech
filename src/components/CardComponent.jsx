78aa3a21a778931e5a0f2d930eb31b69726ba697
hGauna <undrbug@gmail.com>
2025-05-28 17:12:48 -0300

    redireccion a la descripcion del producto al hacer click

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,49 +1,58 @@
 import { Card, Button } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useProductos } from './ProductContext';
 
-const CardComponent = ({onAddToCart}) => {
-    const [productos, setProductos] = useState([]);
-    const [loading, setLoading] = useState(true);
-    // const [text, setText] = useState(buttonText);
+const CardComponent = ({ onAddToCart }) => {
+    const { productos, loading } = useProductos();
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                //Lo tengo que llevar al archivo env
-                const response = await fetch('https://fakestoreapi.com/products');
-                const data = await response.json();
-                setProductos(data);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
+    const handleDetails = (id) => {
+        navigate(`/product/${id}`);
+    };
+    if (loading) {
+        return <div>Cargando...</div>;
     }
-    , []);
+    if (productos.length === 0) {
+        return <div>No hay productos disponibles.</div>;
+    }
+
 
     return (
-        <div className="d-flex flex-column min-vh-100">
-            {loading ? (
-                <div>Cargando...</div>
-            ) : (
-                <div className=" d-flex flex-wrap justify-content-center gap-3 m-3">
+        <>
+            <div className="d-flex flex-column min-vh-100">
+                {loading ? (
+                    <div>Cargando...</div>
+                ) : (
+                    <div className=" d-flex flex-wrap justify-content-center gap-3 m-3">
 
-                    {productos.map((producto) => (
-                        <div key={producto.id}>
-                            <Card style={{ width: '12rem' }}>
-                                <Card.Img className='card-img' variant="top" src={producto.image} />
-                                <Card.Body>
-                                    <Card.Title className='card-title'>{producto.title}</Card.Title>
-                                    <Card.Text>{"usd " + producto.price}</Card.Text>
-                                </Card.Body>
-                                <Button className='btn-agregar'  onClick={() => onAddToCart(producto)}>Agregar al carrito</Button>
-                            </Card>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+                        {productos.map((producto) => (
+                            <div key={producto.id}>
+
+                                <Link to={`/product/${producto.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <Card style={{ width: '12rem' }}>
+                                        <Card.Img className='card-img' variant="top" src={producto.image} />
+                                        <Card.Body>
+                                            <Card.Title className='card-title'>{producto.title}</Card.Title>
+                                            <Card.Text>{"usd " + producto.price}</Card.Text>
+                                        </Card.Body>
+                                        <Button
+                                            className='btn-agregar'
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                e.stopPropagation();
+                                                onAddToCart(producto);
+                                            }}
+                                        >
+                                            Agregar al carrito
+                                        </Button>
+                                    </Card>
+                                </Link >
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
