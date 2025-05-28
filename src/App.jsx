c131379a2a25371b8040b0812b14f13e862d19d0
hGauna <undrbug@gmail.com>
2025-05-28 18:31:29 -0300

    envolvimos las rutas con contexto del carrito

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './components/Dashboard.jsx';
 import Register from './components/Register.jsx';
 import Shop from './components/Shop.jsx';
 import { ProductProvider } from './components/ProductContext.jsx';
+import { CartProvider } from './components/CartContext.jsx';
 import ProductDetail from './components/ProductDetail.jsx';
 import './App.css'
 
@@ -15,38 +16,31 @@ function App() {
 
   return (
     <ProductProvider>
-      <Router>
-        <div className="Container">
-          <h3>BienHelada.com.ar</h3>
-        </div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/deals" element={<Deals />} /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/shop" element={<Shop />} />
+      <CartProvider>
+        <Router>
+          <div className="Container">
+            <h3>BienHelada.com.ar</h3>
+          </div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/deals" element={<Deals />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/shop" element={<Shop />} />
 
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* ruta privada, falta implementar */}
-          {/* <Route path="/checkout" element={<Checkout />} /> */}
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        <Footer />
-      </Router>
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* ruta privada, falta implementar */}
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </CartProvider>
     </ProductProvider>
   )
 }
 
 export default App
-
-// <Main>
-//   <Section title="Bienvenidos a BienHelada.com.ar">
-//     <Article content={<Card />} />
-//     <Article content={<Card />} />
-//     <Article content={<Card />} />
-//     <Article content={<Card />} />
-//   </Section>
-// </Main>
