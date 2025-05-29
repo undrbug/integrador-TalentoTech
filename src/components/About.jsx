d50faef20627abcd0a8beae2fd5ad8cfa4116db3
hGauna <undrbug@gmail.com>
2025-05-28 22:39:48 -0300

    implementacion componente About

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Acerca de</h1>
+      <p>Esta es la página de información sobre nuestra aplicación.</p>
+    </div>
+  );
+}
+
+export default About;
