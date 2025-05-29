a189e63f9c3a3e18e1c0dc16b0a62fd06f900eca
hGauna <undrbug@gmail.com>
2025-05-28 22:51:05 -0300

    navbar, rutas accesibles dependiendo de isAuthenticated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,37 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
-const Header = () => (
-  <header>
-    <Navbar bg="dark" variant="dark" expand="lg" className='radius'>
-      <Container>
-        <Navbar.Brand as={Link} to="/">(logo)</Navbar.Brand>
-        <Navbar.Toggle aria-controls="main-navbar" />
-        <Navbar.Collapse id="main-navbar">
-          <Nav className="justify-content-end flex-grow-1">
-            <Nav.Link as={Link} to="/deals">Ofertas</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
-            <Nav.Link as={Link} to="/about">Acerca de</Nav.Link>
-            <Nav.Link as={Link} to="/login">Iniciar Sesión</Nav.Link>
-            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </header>
-);
+const Header = () => {
+  const { isAuthenticated, logout } = useAuth();
+
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" className='radius'>
+        <Container>
+          <Navbar.Brand as={Link} to="/">(logo)</Navbar.Brand>
+          <Navbar.Toggle aria-controls="main-navbar" />
+          <Navbar.Collapse id="main-navbar">
+            <Nav className="justify-content-end flex-grow-1">
+              <Nav.Link as={Link} to="/deals">Ofertas</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+              <Nav.Link as={Link} to="/about">Acerca de</Nav.Link>
+
+              {isAuthenticated ? (
+                <>
+                  <Nav.Link as={Link} to="/shop">Tienda</Nav.Link>
+                  <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                  <Nav.Link onClick={logout}>Cerrar Sesión</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link as={Link} to="/login">Iniciar Sesión</Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
 
 export default Header;
