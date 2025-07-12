7707c453c9edc5ec32d56eacb1879a08f995e230
hGauna <undrbug@gmail.com>
2025-07-11 22:11:59 -0300

    add component para mostrar ofertas

---
+++ src/components/Deals.jsx
@@ -0,0 +1,17 @@
+//componente que maneja las ofertas
+import React from 'react';
+
+const Deals = () => {
+  return (
+    <div>
+      <h2>Ofertas Especiales</h2>
+      {/* Aquí puedes agregar la lógica para mostrar las ofertas */}
+        <p>Próximamente, ofertas especiales en productos seleccionados.</p>
+      {/* Puedes agregar más contenido, como una lista de productos en oferta */}
+      <p>¡Mantente atento a nuestras promociones!</p>
+      {/* Aquí podrías incluir un componente para mostrar productos en oferta */}
+    </div>
+  );
+}
+
+export default Deals;
