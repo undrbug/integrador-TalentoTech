3841bbf998fa648e54a3bc9578c095cd73fdebde
hGauna <undrbug@gmail.com>
2025-07-11 22:09:53 -0300

    cambio de ruta a la api nueva de productos

--- src/components/ProductContext.jsx
+++ src/components/ProductContext.jsx
@@ -7,14 +7,17 @@ export const useProductos = () => useContext(ProductosContext);
 export const ProductProvider = ({ children }) => {
     const [productos, setProductos] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchProductos = async () => {
             try {
-                const res = await fetch('https://fakestoreapi.com/products');
+                const res = await fetch('http://localhost:3000/api/products');
                 const data = await res.json();
                 setProductos(data);
+                setError(null);
             } catch (error) {
+                setError('Error al cargar productos');
                 console.error('Error al cargar productos', error);
             } finally {
                 setLoading(false);
@@ -24,7 +27,7 @@ export const ProductProvider = ({ children }) => {
     }, []);
 
     return (
-        <ProductosContext.Provider value={{ productos, setProductos, loading }}>
+        <ProductosContext.Provider value={{ productos, setProductos, loading, error }}>
             {children}
         </ProductosContext.Provider>
     );
