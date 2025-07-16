5314bb96ebdc2ff9e2e58962ed34ca54b6f1ca05
hGauna <undrbug@gmail.com>
2025-07-15 21:49:12 -0300

    implementacion de manejo de accesToken al header

--- src/apiService.js
+++ src/apiService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: 'https://api-auth-secure.onrender.com/api', 
   withCredentials: true, // Permite que Axios envíe cookies (para el refreshToken)
 });
 
-// añade el accessToken al header.
+// Es para agregar el accessToken al header.
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
@@ -17,8 +17,8 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-export const registerUser = (email, password) => 
-  api.post('/auth/register', { email, password });
+export const registerUser = (name, email, password) => 
+  api.post('/auth/register', { name, email, password });
 
 export const loginUser = (email, password) => 
   api.post('/auth/login', { email, password });
@@ -31,5 +31,36 @@ export const getMyProfile = () =>
 
 export const refreshToken = () => 
   api.post('/auth/refresh');
+
+export const getProducts = () => 
+  api.get('/products');
+
+export const getProductDeals = () => {
+  return api.get('/products/deals');
+}
+
+export const getProductById = (id) => 
+  api.get(`/products/${id}`);
+
+export const createProduct = (product) => 
+  api.post('/products', product);
+
+export const updateProduct = (id, product) => 
+  api.put(`/products/${id}`, product);
+
+export const deleteProduct = (id) => 
+  api.delete(`/products/${id}`);
+
+export const getUsers = () => 
+  api.get('/users');
+
+export const getUserById = (id) => 
+  api.get(`/users/${id}`);
+
+export const updateUser = (id, user) => 
+  api.put(`/users/${id}`, user);
+
+export const deleteUser = (id) => 
+  api.delete(`/users/${id}`);
 
 export default api;
