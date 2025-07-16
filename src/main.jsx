337fe1a160262c0a349ab9042092c8497aba74be
hGauna <undrbug@gmail.com>
2025-07-15 21:46:45 -0300

    implementacion de toastContainer al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
+import { AuthProvider } from './components/AuthContext.jsx';
+
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <App />
-  </StrictMode>,
+      <AuthProvider>
+        <>
+        <App />
+        <ToastContainer />
+        </>
+      </AuthProvider>
 )
